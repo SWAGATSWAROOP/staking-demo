2323d83c87abb938ab8b837121889671fa88283a
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-02-27 18:58:22 +0530

    Update hardhat.config.js with network settings and gas reporter options

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,52 +1,48 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config()
+require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
-
   solidity: "0.8.6",
-  settings:{
-    optimizer:{
-      enabled:true,
-      runs:200
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
     },
-  
-  networks:{
-    hardhat:{
-      chainId:1337
+
+    networks: {
+      hardhat: {
+        chainId: 1337, //Local chain id for hardhat
+      },
+      // mumbai:{
+      //   url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
+      //   accounts:[process.env.MAIN_ACCOUNT],
+      //   chainId:80001,
+      // },
+
+      //go to metamask and select mumbai testnet
+      //go to account details from the three dots option
+      //select export private key
+      //copy and paste pvt key .env Main_account
     },
-    // mumbai:{
-    //   url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
-    //   accounts:[process.env.MAIN_ACCOUNT],
-    //   chainId:80001,
-    // },
- 
-    //go to metamask and select mumbai testnet
-    //go to account details from the three dots option
-    //select export private key
-    //copy and paste pvt key .env Main_account 
+  },
+  // goerli:{
+  //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API}`,
+  //   accounts:[process.env.MAIN_ACCOUNT],
+  //   chainId: 5,
+  // },
+  // mumbai:{
+  //   url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
+  //   accounts:[process.env.MAIN_ACCOUNT],
+  //   chainId:80001,
+  // },
 
-  }
-},
-    // goerli:{
-    //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API}`,
-    //   accounts:[process.env.MAIN_ACCOUNT],
-    //   chainId: 5,
-    // },
-    // mumbai:{
-    //   url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
-    //   accounts:[process.env.MAIN_ACCOUNT],
-    //   chainId:80001,
-    // },
-
-
-  
-  gasReporter:{
-    enabled:true,
-    currency:"INR",
-    coinmarketcap:process.env.COINMARKETCAP,
-    token:"matic",
-    outputFile:"gasReports.txt",
-    noColors:true
-  }
-}
+  gasReporter: {
+    enabled: true,
+    currency: "INR",
+    coinmarketcap: process.env.COINMARKETCAP,
+    token: "matic",
+    outputFile: "gasReports.txt",
+    noColors: true,
+  },
+};
