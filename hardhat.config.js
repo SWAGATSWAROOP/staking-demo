92d77f35fb0e1f959256a3c3ecd2c40fed8bc107
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-03-03 01:26:22 +0530

    Enable Mumbai testnet configuration in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,29 +14,17 @@ module.exports = {
       hardhat: {
         chainId: 1337, //Local chain id for hardhat
       },
-      // mumbai:{
-      //   url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
-      //   accounts:[process.env.MAIN_ACCOUNT],
-      //   chainId:80001,
-      // },
-
+      mumbai: {
+        url: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
+        accounts: [process.env.MAIN_ACCOUNT],
+        chainId: 80001,
+      },
       //go to metamask and select mumbai testnet
       //go to account details from the three dots option
       //select export private key
       //copy and paste pvt key .env Main_account
     },
   },
-  // goerli:{
-  //   url: `https://goerli.infura.io/v3/${process.env.INFURA_API}`,
-  //   accounts:[process.env.MAIN_ACCOUNT],
-  //   chainId: 5,
-  // },
-  // mumbai:{
-  //   url:`https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API}`,
-  //   accounts:[process.env.MAIN_ACCOUNT],
-  //   chainId:80001,
-  // },
-
   gasReporter: {
     enabled: true,
     currency: "INR",
